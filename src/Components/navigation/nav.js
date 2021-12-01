b045cb2696e849e52819db3492988419b674efea
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-12-01 10:23:46 -0700

    changing the name of the logo

--- src/Components/navigation/nav.js
+++ src/Components/navigation/nav.js
@@ -2,11 +2,14 @@ import './nav.css';
 
 function Navigator() {
     return (
-        <div className='nav'>
+        <><div className='nav'>
             <nav>About</nav>
             <nav>Projects</nav>
             <nav>Contact</nav>
         </div>
+        <div>
+              <img src='src\Components\navigation\android-chrome-512x512.png' alt='logo' />
+        </div></>
     )
 }
 
