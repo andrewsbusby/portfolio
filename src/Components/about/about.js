519e9369392066b32012c8a281824d7cac6b49c0
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-12-28 11:52:24 -0700

    new folder to hold about section

---
+++ src/Components/about/about.js
@@ -0,0 +1,18 @@
+
+
+function About() {
+    return(
+        
+        <div className='about'>
+        <h1>About</h1>
+        <p className= 'aboutMe'>I am a full stack web developer. I have an interest in UI/UX design. I have a firm understanding of HTML, CSS, JavaScript as well as a a few libraries like React, React Redux for frontend.
+        With the backend I know how use Node.js, Express and creating and using RESTful APIs.
+
+        With a year of programming I have a firm understanding in the life cycle of a website and apps. I have experience working with a team remotely to build single page applications. Using GitHub as our version control as well as a way to communicate about in pull requests and any changes that we might see.
+        I am in a position currently where I can take my skills and knowledge as an entry-level software developer.</p>
+        </div>
+        
+    )
+}
+
+export default About;
