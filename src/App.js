fc2cbf159d153ecee687ec48b3dfd977829e69c6
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-12-28 11:50:28 -0700

    update to app.js and about.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Nav from './Components/navigation/nav';
+import About from './Components/about';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Nav />
+        <About />
       </header>
     </div>
   );
